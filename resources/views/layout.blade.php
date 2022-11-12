5a10b0aba3a4f6d9009d4b97d6ca0afb8a37e439
0OSU0 <victor.chino121@gmail.com>
2022-11-12 16:04:15 -0600

    Create layout.blade.php
    Se agrega el layout el cual contiene la vista de la barra de navegacion asi como el contenido el cual es el login y el registro

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>El Iguano</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <style type="text/css">
+        @import url(https://fonts.googleapis.com/css?family=Raleway:300,400,600);
+
+        body{
+            margin: 0;
+            font-size: .9rem;
+            font-weight: 400;
+            line-height: 1.6;
+            color: #212529;
+            text-align: left;
+            background-color: #f5f8fa;
+        }
+        .navbar-laravel
+        {
+            box-shadow: 0 2px 4px rgba(0,0,0,.04);
+        }
+
+        #conte1{
+            background-color: rgb(147, 239, 152);
+        }
+        #conten2{
+            background-color: rgb(147, 239, 152);
+        }
+        .navbar-brand , .nav-link, .my-form, .login-form
+        {
+            font-family: Raleway, sans-serif;
+        }
+        .my-form
+        {
+            padding-top: 1.5rem;
+            padding-bottom: 1.5rem;
+        }
+        .my-form .row
+        {
+            margin-left: 0;
+            margin-right: 0;
+        }
+        .login-form
+        {
+            padding-top: 1.5rem;
+            padding-bottom: 1.5rem;
+        }
+        .login-form .row
+        {
+            margin-left: 0;
+            margin-right: 0;
+        }
+    </style>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
+    <div id="conte1" class="container">
+        <a class="navbar-brand" href="layout"><img src="logo_eiguano.png" alt="logo_eliguano">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">Register</a>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('logout') }}">Logout</a>
+                    </li>
+                @endguest
+            </ul>
+
+        </div>
+    </div>
+</nav>
+
+@yield('content')
+
+</body>
+</html>
