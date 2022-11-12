446d0e6a80f7432bca21d3f1b2fb53b36649929a
0OSU0 <victor.chino121@gmail.com>
2022-11-12 16:07:12 -0600

    Update web.php
    Se agregan rutas al archivo web.php, para usar el controlador y ver las vistas que se han colocado

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+//se agrega el controlador para usar el contenido de este.
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+//se agregan las rutas para las vistas del controlador del auth
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post');
+Route::get('registration', [AuthController::class, 'registration'])->name('register');
+Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post');
+Route::get('dashboard', [AuthController::class, 'dashboard']);
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/', function () {
     return view('welcome');
