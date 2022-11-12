1ddbdd7ffda7da1c88d439419cf824da64e91ce5
ObedGC1705 <castroobed396@gmail.com>
2022-11-12 17:21:22 -0600

    ObedGC1705
    actualizacion de acceso a estilos ccss y funciones js

--- resources/views/categorias.php
+++ resources/views/categorias.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="stylesheet" href="./estilos.css">
+	<link rel="stylesheet" href="./js/estilos.css">
 	<title>Categorias</title>
 </head>
 <body>
@@ -158,6 +158,6 @@
 	</main>
 	
 	<script src="https://kit.fontawesome.com/2c36e9b7b1.js"></script>
-	<script src="./main.js"></script>
+	<script src="./js/main.js"></script>
 </body>
 </html>
