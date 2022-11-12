662b6478e74066718b6856307fc140038721bcb4
ObedGC1705 <castroobed396@gmail.com>
2022-11-12 17:33:29 -0600

    ObedGC1705
    MODIFICACION RUTA CSS

--- resources/views/categorias.php
+++ resources/views/categorias.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="stylesheet" href="/resources/js/estilos.css">
+	<link rel="stylesheet" type="text/css" href="/resources/js/estilos.css">
 	<title>Categorias</title>
 </head>
 <body>
