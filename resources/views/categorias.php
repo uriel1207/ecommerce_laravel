33fc5de8246253729a0b25394d94783848590703
0OSU0 <victor.chino121@gmail.com>
2023-01-07 08:40:57 -0600

    Update categorias.php

--- resources/views/categorias.php
+++ resources/views/categorias.php
@@ -6,6 +6,403 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" type="text/css" href="resources\css\estilos.css">
 	<title>Categorias</title>
+    <style>
+        * {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+
+body {
+	background: #EAEDED;
+	font-family: Arial, sans-serif;
+}
+
+a {
+	text-decoration: none;
+}
+
+.contenedor {
+	margin: auto;
+	width: 90%;
+	max-width: 1200px;
+}
+
+main article {
+	background: #fff;
+	margin: 20px 0;
+	padding: 20px;
+	height: 1000px;
+}
+
+/* ------------------------- */
+/* Enlaces del NAV */
+/* ------------------------- */
+.menu {
+	background: #232F3E;
+	padding: 5px 0;
+	margin-bottom: 20px;
+}
+
+.menu .contenedor-botones-menu {
+	/* display: flex; */
+	display: none;
+	justify-content: space-between;
+}
+
+.menu .contenedor-botones-menu button {
+	font-size: 20px;
+	color: #fff;
+	padding: 10px 20px;
+	border: 1px solid transparent;
+	display: inline-block;
+	cursor: pointer;
+	background: none;
+}
+
+.menu .contenedor-botones-menu button:hover {
+	border: 1px solid rgba(255,255,255, .4);
+}
+
+.menu .contenedor-botones-menu .btn-menu-cerrar {
+	display: none;
+}
+
+.menu .contenedor-botones-menu .btn-menu-cerrar.activo {
+	display: inline-block;
+}
+
+.menu .contenedor-enlaces-nav {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+}
+
+.menu .btn-departamentos {
+	color: #fff;
+	padding: 7px;
+	border-radius: 3px;
+	display: flex;
+	align-items: flex-end;
+	border: 1px solid transparent;
+	font-size: 12px;
+	cursor: default;
+}
+
+.menu .btn-departamentos i {
+	margin-left: 20px;
+	position: relative;
+	bottom: 3px;
+}
+
+.menu .btn-departamentos:hover {
+	border: 1px solid rgba(255,255,255, .4);
+}
+
+.menu .btn-departamentos span {
+	display: block;
+	font-size: 14px;
+	font-weight: bold;
+}
+
+.menu .contenedor-enlaces-nav .enlaces a {
+	color: #ccc;
+	border: 1px solid transparent;
+	padding: 7px;
+	border-radius: 3px;
+	font-size: 14px;
+}
+
+.menu .contenedor-enlaces-nav .enlaces a:hover {
+	border: 1px solid rgba(255,255,255,.4);
+}
+
+/* ------------------------- */
+/* GRID */
+/* ------------------------- */
+.contenedor-grid {
+	position: relative;
+}
+
+.grid {
+	width: 100%;
+	display: none;
+	position: absolute;
+	top: 5px;
+	z-index: 1000;
+	grid-template-columns: repeat(4, 1fr);
+	grid-template-rows: 1fr;
+	grid-template-areas: "categorias subcategorias subcategorias subcategorias";
+}
+
+.grid.activo {
+	display: grid;
+}
+
+.grid::before {
+	content: "";
+	display: block;
+	background: transparent;
+	border-left: 5px solid transparent;
+	border-right: 5px solid transparent;
+	border-bottom: 5px solid #fff;
+	position: absolute;
+	top: -5px;
+	left: 65px;
+}
+
+.grid > div {
+	background: #fff;
+	box-shadow: 0px 3px 6px rgba(0,0,0,.10);
+}
+
+/* ------------------------- */
+/* Categorias */
+/* ------------------------- */
+
+.grid .btn-regresar {
+	background: #232F3E;
+	color: #fff;
+	border: none;
+	padding: 10px;
+	border-radius: 3px;
+	margin: 20px;
+	font-size: 16px;
+	cursor: pointer;
+	display: none;
+}
+
+.grid .btn-regresar i {
+	margin-right: 10px;
+}
+
+.grid .categorias {
+	grid-area: categorias;
+	padding-top: 10px;
+	min-height: 300px;
+	overflow: auto;
+}
+
+.grid .categorias .subtitulo {
+	display: none;
+}
+
+.grid .categorias a {
+	color: #000;
+	font-size: 14px;
+	display: flex;
+	justify-content: space-between;
+	padding: 10px 20px;
+}
+
+.grid .categorias a i {
+	display: none;
+}
+
+.grid .categorias a:hover {
+	color: #E47911;
+	font-weight: bold;
+}
+
+.grid .categorias a:hover i {
+	display: inline-block;
+}
+
+/* ------------------------- */
+/* Subcategorias */
+/* ------------------------- */
+
+.grid .contenedor-subcategorias {
+	grid-area: subcategorias;
+}
+
+.grid .subcategoria {
+	display: none;
+	grid-template-columns: 1fr 1fr 1fr;
+	grid-template-rows: 1fr;
+	height: 100%;
+}
+
+.grid .subcategoria.activo {
+	display: grid;
+}
+
+.grid .subcategoria img {
+	width: 100%;
+	vertical-align: top;
+	height: 100%;
+	object-fit: cover;
+}
+
+.grid .enlaces-subcategoria {
+	padding-top: 10px;
+	height: 100%;
+	max-height: 300px;
+	overflow: auto;
+}
+
+.grid .enlaces-subcategoria a {
+	color: #000;
+	display: block;
+	font-size: 14px;
+	padding: 10px 20px;
+}
+
+.grid .enlaces-subcategoria a:hover {
+	color: #E47911;
+}
+
+.grid .subtitulo {
+	font-size: 18px;
+	font-weight: normal;
+	color: #E47911;
+	padding: 10px 20px;
+}
+
+.grid .banner-subcategoria a {
+	display: block;
+	height: 100%;
+}
+
+.grid .galeria-subcategoria {
+	display: flex;
+	flex-wrap: wrap;
+	align-content: start;
+}
+
+.grid .galeria-subcategoria a {
+	width: 50%;
+	height: 50%;
+}
+
+/* ------------------------- */
+/* Mediaqueies */
+/* ------------------------- */
+@media screen and (max-width: 1000px) {
+	.menu .contenedor-enlaces-nav {
+		padding: 0 10px;
+	}
+
+	.menu .contenedor {
+		width: 100%;
+	}
+}
+
+@media screen and (max-width: 800px) {
+	.menu .contenedor {
+		width: 90%;
+	}
+
+	.menu .contenedor-botones-menu {
+		display: flex;
+	}
+
+	.menu .contenedor-enlaces-nav {
+		padding: 20px;
+		flex-direction: column;
+		align-items: flex-start;
+		justify-content: flex-start;
+		position: fixed;
+		left: 0;
+		background: #232F3F;
+		height: 100vh;
+		width: 80%;
+		z-index: 2000;
+		transition: .3s ease all;
+		transform: translate(-100%);
+	}
+
+	.menu .contenedor-enlaces-nav.activo {
+		transform: translate(0%);
+	}
+
+	.menu .btn-departamentos {
+		width: 100%;
+		align-items: center;
+		justify-content: space-between;
+		cursor: pointer;
+	}
+
+	.menu .btn-departamentos i {
+		position: static;
+		margin-left: 20px;
+		transform: rotate(-90deg);
+	}
+
+	.menu .enlaces {
+		width: 100%;
+	}
+
+	.menu .enlaces a {
+		display: block;
+		margin: 10px 0;
+		padding: 10px;
+	}
+
+	.grid {
+		display: grid;
+		transition: .3s ease all;
+		transform: translateX(-100%);
+		z-index: 3000;
+		width: 80%;
+		position: fixed;
+		top: 0;
+		left: 0;
+		height: 100vh;
+		grid-template-areas: "categorias categorias categorias categorias";
+	}
+
+	.grid.activo {
+		transform: translateX(0%);
+	}
+
+	.grid::before {
+		display: none;
+	}
+
+	.grid .btn-regresar {
+		display: inline-block;
+	}
+
+	.grid .categorias .subtitulo {
+		display: block;
+	}
+
+	.grid .contenedor-subcategorias {
+		position: absolute;
+		top: 0;
+		width: 100%;
+		height: 100%;
+		transition: .3s ease all;
+		transform: translateX(-100%);
+		overflow: auto;
+	}
+
+	.grid .contenedor-subcategorias.activo {
+		transform: translateX(0%);
+	}
+
+	.grid .contenedor-subcategorias .subcategoria {
+		grid-template-columns: 1fr;
+	}
+
+	.grid .contenedor-subcategorias .banner-subcategoria {
+		width: 100%;
+		min-height: 250px;
+		max-height: 350px;
+	}
+
+	.grid .enlaces-subcategoria {
+		min-height: 50vh;
+		overflow: auto;
+	}
+
+	.grid .contenedor-subcategorias .galeria-subcategoria a {
+		width: 25%;
+		height: 100%;
+	}
+}
+    </style>
 </head>
 <body>
 	<nav class="menu" id="menu">
@@ -40,7 +437,7 @@
 					<a href="#" data-categoria="salud-y-belleza">Salud y Belleza <i class="fas fa-angle-right"></i></a>
 					<a href="#" data-categoria="electronica">Electronica <i class="fas fa-angle-right"></i></a>
 					<a href="#" data-categoria="Deportes-y-Exterior">Deportes y Exterior <i class="fas fa-angle-right"></i></a>
-				
+
 				</div>
 
 				<div class="contenedor-subcategorias">
@@ -68,7 +465,7 @@
 							<a href="#">Miedo</a>
 						</div>
 
-						
+
 					</div>
 
 					<div class="subcategoria" data-categoria="ropa-y-accesorios">
@@ -81,7 +478,7 @@
 							<a href="#">Relojes</a>
 						</div>
 
-						
+
 					</div>
 
 					<div class="subcategoria" data-categoria="hogar-y-cocina">
@@ -98,7 +495,7 @@
 							<a href="#">Jardin</a>
 						</div>
 
-						
+
 					</div>
 
 					<div class="subcategoria" data-categoria="juegos-y-juguetes">
@@ -156,8 +553,96 @@
 	<main class="contenedor">
 		<article></article>
 	</main>
-	
+
 	<script src="https://kit.fontawesome.com/2c36e9b7b1.js"></script>
-	<script src="resources\js\main.js"></script>
+	<script>
+        const btnDepartamentos = document.getElementById('btn-departamentos'),
+	  btnCerrarMenu = document.getElementById('btn-menu-cerrar'),
+	  grid = document.getElementById('grid'),
+	  contenedorEnlacesNav = document.querySelector('#menu .contenedor-enlaces-nav'),
+	  contenedorSubCategorias = document.querySelector('#grid .contenedor-subcategorias'),
+	  esDispositivoMovil = () => window.innerWidth <= 800;
+
+btnDepartamentos.addEventListener('mouseover', () => {
+	if(!esDispositivoMovil()){
+		grid.classList.add('activo');
+	}
+});
+
+grid.addEventListener('mouseleave', () => {
+	if(!esDispositivoMovil()){
+		grid.classList.remove('activo');
+	}
+});
+
+document.querySelectorAll('#menu .categorias a').forEach((elemento) => {
+	elemento.addEventListener('mouseenter', (e) => {
+		if(!esDispositivoMovil()){
+			document.querySelectorAll('#menu .subcategoria').forEach((categoria) => {
+				categoria.classList.remove('activo');
+				if(categoria.dataset.categoria == e.target.dataset.categoria){
+					categoria.classList.add('activo');
+				}
+			});
+		};
+	});
+});
+
+// EventListeners para dispositivo movil.
+document.querySelector('#btn-menu-barras').addEventListener('click', (e) => {
+	e.preventDefault();
+	if(contenedorEnlacesNav.classList.contains('activo')){
+		contenedorEnlacesNav.classList.remove('activo');
+		document.querySelector('body').style.overflow = 'visible';
+	} else {
+		contenedorEnlacesNav.classList.add('activo');
+		document.querySelector('body').style.overflow = 'hidden';
+	}
+});
+
+// Click en boton de todos los departamentos (Para version movil).
+btnDepartamentos.addEventListener('click', (e) => {
+	e.preventDefault();
+	grid.classList.add('activo');
+	btnCerrarMenu.classList.add('activo');
+});
+
+// Boton de regresar en el menu de categorias
+document.querySelector('#grid .categorias .btn-regresar').addEventListener('click', (e) => {
+	e.preventDefault();
+	grid.classList.remove('activo');
+	btnCerrarMenu.classList.remove('activo');
+});
+
+document.querySelectorAll('#menu .categorias a').forEach((elemento) => {
+	elemento.addEventListener('click', (e) => {
+		if(esDispositivoMovil()){
+			contenedorSubCategorias.classList.add('activo');
+			document.querySelectorAll('#menu .subcategoria').forEach((categoria) => {
+				categoria.classList.remove('activo');
+				if(categoria.dataset.categoria == e.target.dataset.categoria){
+					categoria.classList.add('activo');
+				}
+			});
+		}
+	});
+});
+
+// Boton de regresar en el menu de categorias
+document.querySelectorAll('#grid .contenedor-subcategorias .btn-regresar').forEach((boton) => {
+	boton.addEventListener('click', (e) => {
+		e.preventDefault();
+		contenedorSubCategorias.classList.remove('activo');
+	});
+});
+
+btnCerrarMenu.addEventListener('click', (e)=> {
+	e.preventDefault();
+	document.querySelectorAll('#menu .activo').forEach((elemento) => {
+		elemento.classList.remove('activo');
+	});
+	document.querySelector('body').style.overflow = 'visible';
+});
+    </script>
 </body>
 </html>
